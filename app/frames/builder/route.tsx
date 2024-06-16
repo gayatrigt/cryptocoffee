12de833de926d21add982c4b963ed998e6aa27a2
gayatrigt <taleygayatri@gmail.com>
2024-06-16 23:43:39 +0530

    added image

--- app/frames/builder/route.tsx
+++ app/frames/builder/route.tsx
@@ -30,28 +30,20 @@ const handleRequest = frames(async (ctx) => {
         style={{
           display: "flex",
           flexDirection: "column",
-          width: "70%",
+          backgroundImage: `url("https://cryptocoffee-opal.vercel.app/bg.png")`,
+          height: "588px",
+          width: "1135px",
+          justifyContent: "center",
           alignItems: "center",
-          textAlign: "center",
-          fontSize: "52",
-          fontWeight: "bold"
         }}
       >
-        Fuel your Frames with some Crypto Coffee
-        <span style={{
-          fontSize: "24",
-        }}>
-          Give your audience
-          an easy way to say thanks
-        </span>
       </div>
     ),
     buttons: [
       <Button action="post" target="/builder/route1">
         Get Started
       </Button>,
-    ]
-    ,
+    ],
   };
 });
 
